46aae878a1582834cb13e9b74820c12a74727fa0
Amelie Yu <amelie.m.yu@gmail.com>
2026-02-21 14:54:00 -0600

    header + fonts

---
+++ app/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full sticky p-4 shadow-2xl bg-primary text-white grid grid-cols-2 font-secondary">
+        <h1 class="col-span-1 font-xl font-bold font-primary">Alzheimer's EEG Diagnostic Tool</h1>
+        <div class="col-span-1 flex justify-end gap-3">
+            <div>Home</div>
+            <div>Get Diagnosis</div>
+            <div>About Us</div>
+        </div>
+        
+    </div>
+</template>
