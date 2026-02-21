2640e9a905b8ddc024a2afd851df608f32912ff0
Amelie Yu <amelie.m.yu@gmail.com>
2026-02-21 15:18:28 -0600

    buton

---
+++ app/components/Button.vue
@@ -0,0 +1,9 @@
+<template> 
+    <button class="py-1 px-4 m-2 bg-primary hover:bg-primary-hover rounded-full font-secondary text-white transition text-center"> 
+        {{ text }}
+    </button>
+</template>
+
+<script setup>
+const props = defineProps(['text'])
+</script>
