485426e01f60a557b6b412139aeed8bca2c79226
fegelsamuel <fegelsamuel@gmail.com>
2026-02-21 17:19:56 -0600

    for mika's eyes; todo: make the arrow not teleport when clicked

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,7 +2,7 @@
     <div class="w-full sticky p-4 shadow-2xl bg-primary text-white grid grid-cols-2 font-secondary">
         <h1 class="col-span-1 font-xl font-bold font-primary">Alzheimer's EEG Diagnostic Tool</h1>
         <div class="col-span-1 flex justify-end gap-3">
-            <div>Home</div>
+            <NuxtLink to="/">Home</NuxtLink>
             <div>Roll the Slots</div>
             <div>About Us</div>
         </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-        <EGGHero/>
+    <SMHero/>
 </template>
